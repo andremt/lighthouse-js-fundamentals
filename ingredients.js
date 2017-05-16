fb432833e6de2fbedb04fe18caebab8bd8f903ea
andremt <andre.m.tate@gmail.com>
2017-05-16 13:41:44 +0000

    Add ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i=0
+
+while (i < ingredients.length) {
+		console.log(ingredients[i]);
+		i++;
+}
+
+for (var g = 0; g < ingredients.length; g++) {
+console.log(ingredients[g]);
+}
+
+for (var g = ingredients.length; g >= 0; g--) {
+console.log(ingredients[g]);
+}
